f731d9afec471af1b228d03b75d13563c33c226b
Rahaf alzahrani <s230044976@seu.edu.sa>
2025-08-12 16:43:20 +0300

    Create pre.java

---
+++ pre.java
@@ -0,0 +1,7 @@
+public class PreIncrementExample { public static void main (String [] args){ int i = 5; int result = ++i + 10;
+
+    System.out.println(" The value of i after pre-increment: " +i);
+    
+    System.out.println(" The results of ++i +10: " + result);
+}
+}
